16824b1514d43d3f431547b5cb2d553c3888e15a
Fran McDade <franmcdade@Frans-MBP.hub>
2020-04-02 14:03:31 +1000

    Add a checkbox to filter the display to only required metadata fields. #609.

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -11,7 +11,7 @@ import React from 'react';
 
 // App dependencies
 import {DraftSiteMap} from '../../hooks/draft-siteMap';
-import {MetadataSiteMap} from '../../hooks/metadata-siteMap';
+import {MetadataNavSiteMap} from '../../hooks/metanav-siteMap';
 import {NavSiteMap} from '../../hooks/nav-siteMap';
 import * as NavigationService from '../../utils/navigation.service';
 
@@ -19,7 +19,7 @@ import * as NavigationService from '../../utils/navigation.service';
 import compStyles from './nav.module.css';
 import fontStyles from '../../styles/fontsize.module.css';
 
-const classNames = require('classnames');
+let classNames = require('classnames');
 
 let active;
 let expanded;
@@ -121,7 +121,7 @@ export default (props) => {
 			allPagesSiteMap = NavSiteMap(docPath),
 			documentsInDraftMode = DraftSiteMap(),
 			pagesSiteMap = NavigationService.getPagesSiteMapByEnvironment(allPagesSiteMap, documentsInDraftMode),
-			metaSiteMap = metaNav ? MetadataSiteMap(docPath) : '';
+			metaSiteMap = metaNav ? MetadataNavSiteMap(docPath) : '';
 
 	const nav = metaNav ? pagesSiteMap.concat(metaSiteMap) : docPath ? pagesSiteMap : '';
 
